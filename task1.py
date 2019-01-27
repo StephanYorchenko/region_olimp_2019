a2c8d9a25e28c8ce139dad9c3542006429360731
Stepan Yurchenko <stepan33314@yandex.ru>
2019-01-27 11:17:19 +0500

    Medium solution of first task. (a,l, r <=10^5)

--- task1.py
+++ task1.py
@@ -1,7 +1,5 @@
 l, r, a = [int(input()) for _ in range(3)]
 res = 0
 for j in range(l + 1, r + 1):
-    for i in range(l, j):
-        if not (j - i) % a:
-            res += 1
+    res += (j - l)//a
 print(res)
