961ccca6240e55e70c6ba86d1814fb46746c0f2a
Stepan Yurchenko <stepan33314@yandex.ru>
2019-01-27 11:12:54 +0500

    Very poor solution of first task (for a, l, r<=100)

--- task1.py
+++ task1.py
@@ -1,3 +1,7 @@
-l, r, a = map(int, input().split())
-
-for
+l, r, a = [int(input()) for _ in range(3)]
+res = 0
+for j in range(l + 1, r + 1):
+    for i in range(l, j):
+        if not (j - i) % a:
+            res += 1
+print(res)
